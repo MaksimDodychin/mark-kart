832f4dbd59856d147ed86a27047e5e06a0e355ff
Максим Добычин <dobyvankenoby@gmail.com>
2026-09-05 22:37:02 +0300

    Ракета рядом с Вихрем в выборе пылесоса; шланг канистрового соединён с вершиной трубы и центром корпуса

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// service worker (версия по содержимому: 955e034d13)
+// service worker (версия по содержимому: 3e9de0aac5)
 // страница игры — network-first (всегда свежая при интернете), офлайн — из кэша.
-const CACHE = 'mark-kart-955e034d13';
+const CACHE = 'mark-kart-3e9de0aac5';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-512-maskable.png','./apple-touch-icon.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => {
